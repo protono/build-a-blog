73df359cd0b2d04b5dbbe47c204751b87fd12e21
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-10-27 16:37:53 +0200

    rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://build-a-blog-protono.netlify.app/'
+  // site: 'http://localhost:4321/'
+})
