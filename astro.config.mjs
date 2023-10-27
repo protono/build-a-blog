a768ca07f01a092ac357308e98d9b8c1bd1f513a
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-10-27 18:19:17 +0200

    island (preact)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
+import preact from "@astrojs/preact"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://build-a-blog-protono.netlify.app/'
+  site: 'https://build-a-blog-protono.netlify.app/',
   // site: 'http://localhost:4321/'
+  integrations: [preact()]
 })
